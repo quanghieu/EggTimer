f3215bc104f10e3b9b16db2b804808cf6194d1aa
hnguy140 <hnguy140@ford.com>
2022-05-13 10:23:46 +0700

    Add content intent and fire notification on broadcast receivers

--- app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerFragment.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerFragment.kt
@@ -51,14 +51,24 @@ class EggTimerFragment : Fragment() {
         binding.lifecycleOwner = this.viewLifecycleOwner
 
         // TODO: Step 1.7 call create channel
-
+        createChannel(getString(R.string.egg_notification_channel_id), getString(R.string.egg_notification_channel_name))
         return binding.root
     }
 
     private fun createChannel(channelId: String, channelName: String) {
         // TODO: Step 1.6 START create a channel
-
+        val notificationChannel = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_LOW)
+        } else {
+            TODO("VERSION.SDK_INT < O")
+        }
+        notificationChannel.enableLights(true)
+        notificationChannel.lightColor = Color.RED
+        notificationChannel.enableVibration(true)
+        notificationChannel.description = "Time for breakfast"
         // TODO: Step 1.6 END create a channel
+        val notificationManager = requireActivity().getSystemService(NotificationManager::class.java)
+        notificationManager.createNotificationChannel(notificationChannel)
 
     }
 
